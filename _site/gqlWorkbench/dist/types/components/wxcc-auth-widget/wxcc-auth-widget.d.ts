645346b0a7ca9b6b4dd85dfd445c468d5122820d
kevsimps <kevsimps@cisco.com>
2025-03-21 17:06:02 -0400

    Full replacement

--- _site/gqlWorkbench/dist/types/components/wxcc-auth-widget/wxcc-auth-widget.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class wxccAuth {
-    token: any;
-    expStamp: any;
-    hide: boolean;
-    callbackUrl: string;
-    burp(x: any): Promise<void>;
-    onGetAuth(): void;
-    render(): any[];
-}
